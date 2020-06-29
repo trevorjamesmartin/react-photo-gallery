c9821c3e7f321eb1fadba64fa9ee5425aad12fdc
Trevor Martin <trevor.j.martin@gmail.com>
2020-06-29 19:49:31 -0400

    dim unselected photos, +1 selected click states

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import "./App.css";
 const defaultGallery = [
   { image: "https://placekitten.com/408/287", desc: "a kitten" },
   { image: "https://placekitten.com/200/287", desc: "another kitten" },
+  { image: "https://placekitten.com/200/138", desc: "kitty kat" },
+  { image: "https://placekitten.com/200/140", desc: "meow" },
+  { image: "https://placekitten.com/200/286", desc: "merr" },
 ];
 function App() {
   return (
--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import Photograph from "../photograph";
 import "./gallery.css";
 /**
@@ -21,12 +21,27 @@ const Gallery = ({ photos }) => {
   const [selectedPhoto, setSelectedPhoto] = useState({
     image: NaN,
     desc: NaN,
+    click: 0,
   });
-  const handleSelectPhoto = (e, image, desc) => {
-    e.preventDefault();
-    // console.log(image, desc);
-    setSelectedPhoto({ image, desc });
-  };
+
+  const cbSelectPhoto = useCallback(
+    (e, image, desc) => {
+      e.preventDefault();
+      // console.log(image, desc);
+      const { image: oldImage, desc: oldDesc } = selectedPhoto;
+      if (oldImage === image && oldDesc === desc) {
+        selectedPhoto.click += 1;
+      } else {
+        selectedPhoto.click = 1;
+      }
+      let { click } = selectedPhoto;
+      click = click % 3;
+      setSelectedPhoto({ image, desc, click });
+      console.log(["minimized", "selected", "maximized"][click], desc);
+    },
+    [selectedPhoto]
+  );
+
   useEffect(
     (galleryState) => {
       const updateGallery = () =>
@@ -35,18 +50,31 @@ const Gallery = ({ photos }) => {
             key={i}
             image={photo.image}
             desc={photo.desc}
-            handleClick={handleSelectPhoto}
+            handleClick={cbSelectPhoto}
             isSelected={selectedPhoto.image === photo.image}
+            isExpanded={selectedPhoto.click === 2}
           />
         ));
       setGalleryState({ ...galleryState, collection: updateGallery() });
     },
-    [photos, selectedPhoto.image]
+    [photos, selectedPhoto.image, selectedPhoto.click, cbSelectPhoto]
   );
+  // const handleButton = (e) => {
+  //   e.preventDefault();
+  //   console.log(e.target.name);
+  // };
   return (
-    <div className="gallery">
+    <div className="gallery-frame">
       <h1>{selectedPhoto.image ? selectedPhoto.desc : "Photos"}</h1>
-      {galleryState.collection}
+      <div className="gallery">{galleryState.collection}</div>
+      <div className="gallery-controls">
+        {/* <button name="prev" className="gallery-button" onClick={handleButton}>
+          previous
+        </button>
+        <button name="next" className="gallery-button" onClick={handleButton}>
+          next
+        </button> */}
+      </div>
     </div>
   );
 };
