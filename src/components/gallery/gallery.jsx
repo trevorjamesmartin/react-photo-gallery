9329df0726542801f50947962d0f097f7eb4cef8
Trevor Martin <trevor.j.martin@gmail.com>
2020-06-29 16:23:38 -0400

    selectable

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -17,23 +17,38 @@ import "./gallery.css";
  *  ]
  */
 const Gallery = ({ photos }) => {
-  const [galleryState, setGalleryState] = useState({
-    collection: [],
-    selected: NaN,
-    status: NaN,
+  const [galleryState, setGalleryState] = useState({ collection: [] });
+  const [selectedPhoto, setSelectedPhoto] = useState({
+    image: NaN,
+    desc: NaN,
   });
-
+  const handleSelectPhoto = (e, image, desc) => {
+    e.preventDefault();
+    // console.log(image, desc);
+    setSelectedPhoto({ image, desc });
+  };
   useEffect(
     (galleryState) => {
       const updateGallery = () =>
         photos.map((photo, i) => (
-          <Photograph key={i} image={photo.image} desc={photo.desc} />
+          <Photograph
+            key={i}
+            image={photo.image}
+            desc={photo.desc}
+            handleClick={handleSelectPhoto}
+            isSelected={selectedPhoto.image === photo.image}
+          />
         ));
       setGalleryState({ ...galleryState, collection: updateGallery() });
     },
-    [photos]
+    [photos, selectedPhoto.image]
   );
-  return <div className="gallery">{galleryState.collection}</div>;
+  return (
+    <div className="gallery">
+      <h1>{selectedPhoto.image ? selectedPhoto.desc : "Photos"}</h1>
+      {galleryState.collection}
+    </div>
+  );
 };
 
 export default Gallery;
--- src/components/photograph/photograph.jsx
+++ src/components/photograph/photograph.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./photograph.css";
 /**
  *
  * @param {*} image image file / url
  * @param {*} desc description of photograph
  */
-const Photograph = ({ image, desc }) => {
-  return <img className="photograph" src={image} alt={desc} />;
+const Photograph = ({ image, desc, handleClick, isSelected }) => {
+  const [photoState, setPhotoState] = useState({
+    hover: false,
+    image,
+    desc,
+  });
+  return (
+    <img
+      className={`photograph${photoState.hover ? "-hover" : ""}${
+        isSelected ? " photograph-select" : ""
+      }`}
+      src={image}
+      alt={desc}
+      onMouseOver={() => setPhotoState({ ...photoState, hover: true })}
+      onMouseLeave={() => setPhotoState({ ...photoState, hover: false })}
+      onClick={(e) => handleClick(e, photoState.image, photoState.desc)}
+    />
+  );
 };
 
 export default Photograph;
